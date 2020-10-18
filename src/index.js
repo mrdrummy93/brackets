455c197110b5d39f7131fbd3955805afe8f67b5d
mrdrummy93 <vasya_2393@drummer.ru>
2020-10-18 23:00:31 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+        let stack = [];
+        stack.empty = function () {
+            return stack == 0;
+        }
+        stack.top = function () {
+            return stack[stack.length - 1];
+        }
+        for(let i = 0; i < str.length; i++) {
+            if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+                stack.push(str[i])
+            } else if (str[i] === ')' || str[i] === '}' || str[i] === ']') {
+                if (stack.empty()) {
+                    return false
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.empty();
+
 }
